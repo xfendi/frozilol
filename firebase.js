ee806db6c29289fb38aa085f138a313327676fd8
fendziorr <xfendi.channel@gmail.com>
2025-06-08 17:58:23 +0200

    secured firebase connection keys

--- firebase.js
+++ firebase.js
@@ -2,15 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBxxyXqJuRFJd1p_QuPAAUrwXRTFTfj3Zg",
-  authDomain: "frozilol.firebaseapp.com",
-  projectId: "frozilol",
-  storageBucket: "frozilol.firebasestorage.app",
-  messagingSenderId: "212967781960",
-  appId: "1:212967781960:web:8a99ee107967786ea2ca7d",
-  measurementId: "G-R986G0M9KT",
-};
+import firebaseConfig from "@/firebaseConfig.json";
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
