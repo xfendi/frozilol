20d59a7d25796c9f55bfe4934f4206b699cb55ba
fendziorr <xfendi.channel@gmail.com>
2025-06-16 22:03:41 +0200

    designed dashboard error pages

--- components/dashboard/other/notFound.tsx
+++ components/dashboard/other/notFound.tsx
@@ -1,7 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <section
+      className="form__section flex flex-col gap-[32px] items-center justify-center w-full h-screen"
+      data-aos="fade-up"
+    >
+      <div className="form__top">
+        <Link
+          href="/"
+          className="flex items-center gap-2 transition-all duration-300 hover:scale-120 hover:rotate-12"
+        >
+          <Image
+            src="/images/main-cube.png"
+            alt="frozi.lol"
+            width={50}
+            height={50}
+          />
+        </Link>
+      </div>
+
+      <div className="form__main flex flex-col gap-[16px] items-center text-center justify-center md:max-w-md">
+        <div className="section-title text-2xl sm:text-3xl font-extrabold max-w-5xl">
+          🛠️ Page not found!
+        </div>
+        <div className="section-description !text-stone-500 text-md sm:text-xl">
+          The page you are looking for does not exist or has been moved. Please
+          check the URL or return to the dashboard.
+        </div>
+      </div>
+
+      <div className="form__footer">
+        <div className="form__footer-text">
+          Go back to <Link href="/shop">Dashboard</Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default NotFoundPage;
