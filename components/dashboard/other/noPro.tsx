1af821960f3f5d282f45e2e8590e9921e8d79169
fendziorr <xfendi.channel@gmail.com>
2025-06-17 12:44:39 +0200

    fixed dashboard error pages

--- components/dashboard/other/noPro.tsx
+++ components/dashboard/other/noPro.tsx
@@ -4,36 +4,38 @@ import React from "react";
 
 const NoProPage = () => {
   return (
-    <section
-      className="form__section flex flex-col gap-[32px] items-center justify-center w-full h-screen"
-      data-aos="fade-up"
-    >
-      <div className="form__top">
-        <Link
-          href="/"
-          className="flex items-center gap-2 transition-all duration-300 hover:scale-120 hover:rotate-12"
-        >
-          <Image
-            src="/images/main-cube.png"
-            alt="frozi.lol"
-            width={50}
-            height={50}
-          />
-        </Link>
-      </div>
-
-      <div className="form__main flex flex-col gap-[16px] items-center text-center justify-center md:max-w-md">
-        <div className="section-title text-2xl sm:text-3xl font-extrabold max-w-5xl">
-          💎 You need to be a Pro user to access this page!
+    <section className="flex justify-center items-center w-full h-full">
+      <div
+        className=" flex flex-col gap-[32px] items-center justify-center !w-max !h-max"
+        data-aos="fade-up"
+      >
+        <div className="form__top">
+          <Link
+            href="/"
+            className="flex items-center gap-2 transition-all duration-300 hover:scale-120 hover:rotate-12"
+          >
+            <Image
+              src="/images/main-cube.png"
+              alt="frozi.lol"
+              width={50}
+              height={50}
+            />
+          </Link>
         </div>
-        <div className="section-description !text-stone-500 text-md sm:text-xl">
-          Upgrade to Pro to unlock this feature and many more!
-        </div>
-      </div>
 
-      <div className="form__footer">
-        <div className="form__footer-text">
-          Go back to <Link href="/shop">Dashboard</Link>
+        <div className="form__main flex flex-col gap-[16px] items-center text-center justify-center md:max-w-md">
+          <div className="section-title text-2xl sm:text-3xl font-extrabold max-w-5xl">
+            💎 You need to be a Pro user to access this page!
+          </div>
+          <div className="section-description !text-stone-500 text-md sm:text-xl">
+            Upgrade to Pro to unlock this feature and many more!
+          </div>
+        </div>
+
+        <div className="form__footer">
+          <div className="form__footer-text">
+            Go back to <Link href="/shop">Dashboard</Link>
+          </div>
         </div>
       </div>
     </section>
--- components/dashboard/other/notFound.tsx
+++ components/dashboard/other/notFound.tsx
@@ -4,37 +4,39 @@ import React from "react";
 
 const NotFoundPage = () => {
   return (
-    <section
-      className="form__section flex flex-col gap-[32px] items-center justify-center w-full h-screen"
-      data-aos="fade-up"
-    >
-      <div className="form__top">
-        <Link
-          href="/"
-          className="flex items-center gap-2 transition-all duration-300 hover:scale-120 hover:rotate-12"
-        >
-          <Image
-            src="/images/main-cube.png"
-            alt="frozi.lol"
-            width={50}
-            height={50}
-          />
-        </Link>
-      </div>
-
-      <div className="form__main flex flex-col gap-[16px] items-center text-center justify-center md:max-w-md">
-        <div className="section-title text-2xl sm:text-3xl font-extrabold max-w-5xl">
-          🛠️ Page not found!
+    <section className="flex justify-center items-center w-full h-full">
+      <div
+        className=" flex flex-col gap-[32px] items-center justify-center !w-max !h-max"
+        data-aos="fade-up"
+      >
+        <div className="form__top">
+          <Link
+            href="/"
+            className="flex items-center gap-2 transition-all duration-300 hover:scale-120 hover:rotate-12"
+          >
+            <Image
+              src="/images/main-cube.png"
+              alt="frozi.lol"
+              width={50}
+              height={50}
+            />
+          </Link>
         </div>
-        <div className="section-description !text-stone-500 text-md sm:text-xl">
-          The page you are looking for does not exist or has been moved. Please
-          check the URL or return to the dashboard.
-        </div>
-      </div>
 
-      <div className="form__footer">
-        <div className="form__footer-text">
-          Go back to <Link href="/shop">Dashboard</Link>
+        <div className="form__main flex flex-col gap-[16px] items-center text-center justify-center md:max-w-md">
+          <div className="section-title text-2xl sm:text-3xl font-extrabold max-w-5xl">
+            🛠️ Page not found!
+          </div>
+          <div className="section-description !text-stone-500 text-md sm:text-xl">
+            The page you are looking for does not exist or has been moved.
+            Please check the URL or return to the dashboard.
+          </div>
+        </div>
+
+        <div className="form__footer">
+          <div className="form__footer-text">
+            Go back to <Link href="/shop">Dashboard</Link>
+          </div>
         </div>
       </div>
     </section>
