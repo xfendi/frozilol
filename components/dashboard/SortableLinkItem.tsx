ba0bfde9da4525c111566c89e4a95606dc7d29cc
fendziorr <xfendi.channel@gmail.com>
2025-06-24 20:08:36 +0200

    added profile link edit function

--- components/dashboard/SortableLinkItem.tsx
+++ components/dashboard/SortableLinkItem.tsx
@@ -12,13 +12,17 @@ import {
   updateDoc,
   where,
 } from "firebase/firestore";
-import { GripVertical, Trash } from "lucide-react";
+import { GripVertical, Pencil, Trash } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
 import toast from "react-hot-toast";
+import Modal from "../global/modal";
 
 export const SortableLinkItem = ({ link }: { link: LinkType }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: link.id! });
+
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
   const style = {
     transform: CSS.Transform.toString(transform),
@@ -29,6 +33,14 @@ export const SortableLinkItem = ({ link }: { link: LinkType }) => {
   const linkData = linksMap.find((l) => l.name === link.name);
 
   const cleanLink = link?.link?.replace(/^https?:\/\//, "");
+  const cleanLinkProfile = cleanLink?.replace(
+    new RegExp(`^${linkData?.profileStartLink}`),
+    ""
+  );
+
+  const [editLink, setEditLink] = useState(cleanLinkProfile);
+
+  const [loading, setLoading] = useState(false);
 
   const { user } = AuthData();
 
@@ -59,47 +71,139 @@ export const SortableLinkItem = ({ link }: { link: LinkType }) => {
     }
   };
 
-  return (
-    <div ref={setNodeRef} style={style} className="link-card">
-      <div
-        className="flex items-center justify-center bg-(--border-secondary) rounded-[12px] h-full !p-1 cursor-grab"
-        {...attributes}
-        {...listeners}
-      >
-        <GripVertical color="var(--color-stone-500)" size={16} />
-      </div>
+  const handleEdit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-      <div className="flex flex-col !p-4 flex-1">
-        <div className="input_container">
-          <div className="flex items-center justify-between gap-2">
-            <div className="input_title">{linkData?.friendlyName}</div>
-            <button
-              className="btn-opacity !p-1.5 !rounded-[8px]"
-              onClick={handleDelete}
-            >
-              <Trash color="var(--color-red-500)" size={16} />
-            </button>
-          </div>
-          <div className="input_body !w-full">
-            <Image
-              src={`/${link?.type}/${link?.name}.png`}
-              className="!mr-2"
-              alt={link?.name || ""}
-              width={24}
-              height={24}
-            />
-            {link?.type !== "crypto" && (
-              <span className="!text-stone-500">{link?.profileStartLink}</span>
-            )}
-            <input
-              type="text"
-              placeholder="..."
-              value={cleanLink}
-              className="focus:outline-none !text-stone-300"
-            />
+    if (!editLink) {
+      return toast.error("Please provide a link.");
+    }
+
+    if (editLink === cleanLinkProfile) {
+      return toast.error("You have not changed the link.");
+    }
+
+    setLoading(true);
+
+    try {
+      const q = query(collection(db, "profiles"), where("uid", "==", user.uid));
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.empty) {
+        console.error("User profile not found!");
+        return;
+      }
+
+      const userDocRef = querySnapshot.docs[0].ref;
+      const userDocData = querySnapshot.docs[0].data();
+      const currentLinks = userDocData.links || [];
+
+      const updatedLinks = currentLinks.map((l: LinkType) => {
+        if (l.link === link.link) {
+          return {
+            ...l,
+            link: `https://${linkData?.profileStartLink}${editLink}`,
+          };
+        }
+
+        return l;
+      });
+
+      await updateDoc(userDocRef, { links: updatedLinks });
+
+      toast.success("Link updated successfully!");
+    } catch (error) {
+      console.error("Error updating link:", error);
+    } finally {
+      setLoading(false);
+      setIsEditModalOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <div ref={setNodeRef} style={style} className="link-card">
+        <div
+          className="flex items-center justify-center bg-(--border-secondary) rounded-[12px] h-full !p-1 cursor-grab"
+          {...attributes}
+          {...listeners}
+        >
+          <GripVertical color="var(--color-stone-500)" size={16} />
+        </div>
+
+        <div className="flex flex-col !p-4 flex-1">
+          <div className="input_container">
+            <div className="flex items-center justify-between gap-2">
+              <div className="input_title">{linkData?.friendlyName}</div>
+              <div className="flex gap-1">
+                <button
+                  className="btn-opacity !p-1.5 !rounded-[8px]"
+                  onClick={() => setIsEditModalOpen(true)}
+                >
+                  <Pencil color="var(--color-stone-500)" size={16} />
+                </button>
+                <button
+                  className="btn-opacity !p-1.5 !rounded-[8px]"
+                  onClick={handleDelete}
+                >
+                  <Trash color="var(--color-red-500)" size={16} />
+                </button>
+              </div>
+            </div>
+            <div className="input_body !w-full">
+              <Image
+                src={`/${link?.type}/${link?.name}.png`}
+                className="!mr-2"
+                alt={link?.name || ""}
+                width={24}
+                height={24}
+              />
+              {link?.type !== "crypto" && (
+                <span className="!text-stone-500">
+                  {link?.profileStartLink}
+                </span>
+              )}
+              <input
+                type="text"
+                placeholder="..."
+                value={cleanLink}
+                className="focus:outline-none !text-stone-300"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Modal
+        isOpen={isEditModalOpen}
+        onClose={() => setIsEditModalOpen(false)}
+        title={`Edit ${linkData?.friendlyName}`}
+        content={
+          <div className="input_container">
+            <div className="input_body">
+              <Image
+                src={`/${link?.type}/${link?.name}.png`}
+                className="!mr-2"
+                alt={linkData?.name || ""}
+                width={24}
+                height={24}
+              />
+              {link?.type !== "crypto" && (
+                <span className="!text-stone-500">
+                  {linkData?.profileStartLink}
+                </span>
+              )}
+              <input
+                type="text"
+                placeholder="..."
+                value={editLink}
+                onChange={(e) => setEditLink(e.target.value)}
+                className="focus:outline-none"
+              />
+            </div>
+          </div>
+        }
+        onSubmit={handleEdit}
+        isSending={loading}
+      />
+    </>
   );
 };
